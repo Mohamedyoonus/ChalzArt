42a6abd5ccc3741844e39bc432247039c83d21d2
Mohamed Yoonus <yoonusy655@gmail.com>
2025-05-13 19:20:56 +0530

    footer  update

--- FE/src/components/Footer.jsx
+++ FE/src/components/Footer.jsx
@@ -301,7 +301,7 @@ const Footer = () => {
           }}
         >
           <Typography variant="body2" sx={{ color: "#666", fontSize: 14 }}>
-            © 2019–2035 Chalzart. All rights reserved.
+            © 2019–2025 Chalzart. All rights reserved.
           </Typography>
 
           <Typography variant="body2" sx={{ color: "#666", fontSize: 14 }}>
