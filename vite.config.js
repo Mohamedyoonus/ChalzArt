465a3382cff0f4e49daf108f9b4cd581a94abee6
Mohamed Yoonus <yoonusy655@gmail.com>
2025-10-26 12:20:23 +0530

    enhance mywork page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
+  base: '/', // Move this to top-level
+
   plugins: [
     react(),
     viteStaticCopy({
